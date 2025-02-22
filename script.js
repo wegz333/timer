691b1f3b99b5accc3dd8ce352620ba3d3403cd47
s2002 <133994798+wegz333@users.noreply.github.com>
2025-02-22 17:00:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const timer = setInterval(() => {
     const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000;
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
     // Display the results
     document.getElementById("days").innerText = days;
